9349b622d6cdc58e57a034276c08759846d94c46
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-06-14 11:18:05 -0600

    Add route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get '/plots', to: 'plots#index'
 
   delete '/plots/:plot_id/plants/:plant_id', to: 'plot_plants#destroy'
+
+  get '/gardens/:garden_id', to: 'gardens#show'
 end
